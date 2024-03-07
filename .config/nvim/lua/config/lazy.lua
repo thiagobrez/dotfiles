407999fbff5f5fb7281bec5c1acd23c35a8be349
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-03-07 10:19:43 +0100

    feat: add toggleterm

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -44,3 +44,14 @@ require("lazy").setup({
     },
   },
 })
+
+require('toggleterm').setup {
+  direction = 'vertical',
+  size = function(term)
+    if term.direction == 'horizontal' then
+      return 15
+    elseif term.direction == 'vertical' then
+      return vim.o.columns * 0.3
+    end
+  end,
+}
