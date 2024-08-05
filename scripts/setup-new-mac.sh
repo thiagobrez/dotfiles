8850244b54daf2377963218437e2e9f97206f900
Thiago Brezinski <thiagobrez@gmail.com>
2024-08-05 18:33:21 +0100

    fix: improve initial script

--- scripts/setup-new-mac.sh
+++ scripts/setup-new-mac.sh
@@ -1,22 +1,35 @@
 # Setup macOS preferences
+echo "Changing macOS defaults..."
 ./setup-preferences.sh
+echo "MacOS defaults changed successfully ✅"
 
-# Install brew
+# Install Homebrew
+echo "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Add Homebrew to PATH
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/thiagobrez/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
+echo "Homebrew installed successfully ✅"
 
 # Upgrade brew
+echo "Upgrading Homebrew..."
 brew upgrade
+echo "Homebrew upgraded successfully ✅"
 
-# Install brew packages from Brewfile
+# Install Homebrew packages from Brewfile
+echo "Installing packages from Brewfile..."
 brew bundle
+echo "Homebrew packages installed successfully ✅"
 
-echo "Installed all apps & utils ✅"
+# Install Bun
+echo "Installing Bun..."
+curl -fsSL https://bun.sh/install | bash
+echo "Bun installed successfully ✅"
 
 # Symlink dotfiles to the HOME directory
+echo "Symlinking dotfiles..."
 stow -t ~ -d ~/dotfiles zshrc gitconfig
 
-echo "Symlinked dotfiles ✅" 
+echo "Symlinked dotfiles successfully ✅"
+echo "Installed all apps & utils 🎉"
