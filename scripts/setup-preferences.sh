df01b69386f4a122b5fcad7ce6b2e1845505ffe0
Thiago Brezinski <thiagobrez@gmail.com>
2024-08-05 18:09:28 +0100

    fix: rollback key repetition

--- scripts/setup-preferences.sh
+++ scripts/setup-preferences.sh
@@ -46,10 +46,10 @@ defaults write com.apple.dock dashboard-in-overlay -bool true
 defaults write NSGlobalDomain "ApplePressAndHoldEnabled" -bool "false"
 
 # Keyboard: change initial key repeat time
-defaults write NSGlobalDomain InitialKeyRepeat -int 5
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 # Keyboard: change key repeat time
-defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain KeyRepeat -int 2
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
